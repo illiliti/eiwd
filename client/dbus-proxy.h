c15cdbe753615fd9bd35949d71506ee1f148af28
James Prestwood <prestwoj@gmail.com>
2021-01-19 09:14:44 -0800

    client: add diagnostic interface definition

--- client/dbus-proxy.h
+++ client/dbus-proxy.h
@@ -24,15 +24,16 @@
 
 struct proxy_interface;
 
-#define IWD_ADAPTER_INTERFACE          "net.connman.iwd.Adapter"
-#define IWD_ACCESS_POINT_INTERFACE     "net.connman.iwd.AccessPoint"
-#define IWD_AD_HOC_INTERFACE           "net.connman.iwd.AdHoc"
-#define IWD_AGENT_MANAGER_INTERFACE    "net.connman.iwd.AgentManager"
-#define IWD_DEVICE_INTERFACE           "net.connman.iwd.Device"
-#define IWD_KNOWN_NETWORK_INTREFACE    "net.connman.iwd.KnownNetwork"
-#define IWD_NETWORK_INTERFACE          "net.connman.iwd.Network"
-#define IWD_WSC_INTERFACE              "net.connman.iwd.SimpleConfiguration"
-#define IWD_STATION_INTERFACE          "net.connman.iwd.Station"
+#define IWD_ADAPTER_INTERFACE            "net.connman.iwd.Adapter"
+#define IWD_ACCESS_POINT_INTERFACE       "net.connman.iwd.AccessPoint"
+#define IWD_AD_HOC_INTERFACE             "net.connman.iwd.AdHoc"
+#define IWD_AGENT_MANAGER_INTERFACE      "net.connman.iwd.AgentManager"
+#define IWD_DEVICE_INTERFACE             "net.connman.iwd.Device"
+#define IWD_KNOWN_NETWORK_INTREFACE      "net.connman.iwd.KnownNetwork"
+#define IWD_NETWORK_INTERFACE            "net.connman.iwd.Network"
+#define IWD_WSC_INTERFACE                "net.connman.iwd.SimpleConfiguration"
+#define IWD_STATION_INTERFACE            "net.connman.iwd.Station"
+#define IWD_STATION_DIAGNOSTIC_INTERFACE "net.connman.iwd.StationDiagnostic"
 
 typedef bool (*proxy_property_match_func_t) (const void *a, const void *b);
 
