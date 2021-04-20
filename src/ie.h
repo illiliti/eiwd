illiliti/eiwd

commit e30345d699d309d9e4f7132a87698c78bd8643c1

ie: Add IE_AKM_IS_FILS macro

src/ie.h
@@ -296,6 +296,12 @@ enum ie_rsn_akm_suite {
 	 (akm == IE_RSN_AKM_SUITE_FT_OVER_FILS_SHA256) || \
 	 (akm == IE_RSN_AKM_SUITE_FT_OVER_FILS_SHA384))
 
+#define IE_AKM_IS_FILS(akm) \
+	((akm == IE_RSN_AKM_SUITE_FILS_SHA256) || \
+	 (akm == IE_RSN_AKM_SUITE_FILS_SHA384) || \
+	 (IE_RSN_AKM_SUITE_FT_OVER_FILS_SHA256) || \
+	 (IE_RSN_AKM_SUITE_FT_OVER_FILS_SHA384))
+
 #define IE_LEN(ie) \
 	(ie) ? (ie)[1] + 2 : 0
 

commit 89b3d34dd3fc05ee11dcfc204b48971619969659

ie: Fix IE_AKM_IS_FILS macro
Fixes: e30345d699d3 ("ie: Add IE_AKM_IS_FILS macro")

src/ie.h
@@ -299,8 +299,8 @@ enum ie_rsn_akm_suite {
 #define IE_AKM_IS_FILS(akm) \
 	((akm == IE_RSN_AKM_SUITE_FILS_SHA256) || \
 	 (akm == IE_RSN_AKM_SUITE_FILS_SHA384) || \
-	 (IE_RSN_AKM_SUITE_FT_OVER_FILS_SHA256) || \
-	 (IE_RSN_AKM_SUITE_FT_OVER_FILS_SHA384))
+	 (akm == IE_RSN_AKM_SUITE_FT_OVER_FILS_SHA256) || \
+	 (akm == IE_RSN_AKM_SUITE_FT_OVER_FILS_SHA384))
 
 #define IE_LEN(ie) \
 	(ie) ? (ie)[1] + 2 : 0