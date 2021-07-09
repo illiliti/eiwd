illiliti/eiwd

commit 7a9891dbef5b7b32426541cb2970997a6f5f02f9

wsc: store plain text passphrase if available
If the AP sent us the plain passphrase we can now store that rather
than generating the PSK. This will allow WPA3 to work properly when
WPA3 + WSC is implemented.

src/wsc.c
@@ -63,8 +63,12 @@ struct wsc {
 	struct {
 		char ssid[33];
 		enum security security;
-		uint8_t psk[32];
+		union {
+			uint8_t psk[32];
+			char passphrase[64];
+		};
 		uint8_t addr[6];
+		bool has_passphrase;
 	} creds[3];
 	uint32_t n_creds;
 	struct l_settings *eap_settings;
@@ -132,9 +136,24 @@ static void wsc_try_credentials(struct wsc *wsc)
 		if (!bss)
 			continue;
 
-		if (wsc->creds[i].security == SECURITY_PSK &&
-				!network_set_psk(network, wsc->creds[i].psk))
-			continue;
+		if (wsc->creds[i].security == SECURITY_PSK) {
+			bool ret;
+
+			/*
+			 * Prefer setting passphrase, this will work for both
+			 * WPA2 and WPA3 since the PSK can always be generated
+			 * if needed
+			 */
+			if (wsc->creds[i].has_passphrase)
+				ret = network_set_passphrase(network,
+						wsc->creds[i].passphrase);
+			else
+				ret = network_set_psk(network,
+						wsc->creds[i].psk);
+
+			if (!ret)
+				continue;
+		}
 
 		station_connect_network(wsc->station, network, bss,
 								wsc->pending);
@@ -315,18 +334,10 @@ static void wsc_credential_obtained(struct wsc *wsc,
 			explicit_bzero(decoded, 32);
 			l_free(decoded);
 		} else {
-			const char *passphrase =
-				(const char *) cred->network_key;
-			/*
-			 * wscutil should memset cred->network_key to 0 prior
-			 * to copying in the contents of the passphrase
-			 */
-			if (crypto_psk_from_passphrase(passphrase,
-					cred->ssid, cred->ssid_len,
-					wsc->creds[wsc->n_creds].psk) != 0) {
-				l_warn("Ignoring invalid passphrase");
-				return;
-			}
+			strncpy(wsc->creds[wsc->n_creds].passphrase,
+					(const char *) cred->network_key,
+					cred->network_key_len);
+			wsc->creds[wsc->n_creds].has_passphrase = true;
 		}
 
 		break;

commit 10e5bee5efe480132edae56616d64b672bc7d846

wsc: Properly write provisioning files with a passphrase
Credentials obtained can now be either in passphrase or PSK form.  Prior
to commit 7a9891dbef5b, passphrase credentials were always converted to
PSK form by invoking crypto_psk_from_passphrase.  This was changed in
order to support WPA3 networks.  Unfortunately the provisioning logic
was never properly updated.  Fix that, and also try to not overwrite any
existing settings in case WSC is providing credentials for networks that
are already known.

Fixes: 7a9891dbef5b ("wsc: store plain text passphrase if available")

src/wsc.c
@@ -28,6 +28,7 @@
 #include <stdio.h>
 #include <ell/ell.h>
 
+#include "ell/useful.h"
 #include "src/missing.h"
 #include "src/module.h"
 #include "src/dbus.h"
@@ -40,7 +41,6 @@
 #include "src/util.h"
 #include "src/handshake.h"
 #include "src/eap-wsc.h"
-#include "src/crypto.h"
 #include "src/common.h"
 #include "src/storage.h"
 #include "src/iwd.h"
@@ -525,21 +525,40 @@ static void wsc_store_credentials(struct wsc_credentials_info *creds,
 {
 	unsigned int i;
 
+	/* We don't store any non-open/psk credentials */
 	for (i = 0; i < n_creds; i++) {
 		enum security security = creds[i].security;
 		const char *ssid = creds[i].ssid;
-		struct l_settings *settings = l_settings_new();
+		_auto_(l_settings_free) struct l_settings *settings =
+							l_settings_new();
+		_auto_(l_free) char *path =
+				storage_get_network_file_path(security, ssid);
+
+		if (l_settings_load_from_file(settings, path)) {
+			/*
+			 * Nothing to do,
+			 * so don't overwrite any existing settings
+			 */
+			if (security == SECURITY_NONE)
+				continue;
+
+			/* Remove any existing Security keys */
+			l_settings_remove_group(settings, "Security");
+		}
+
+		if (security == SECURITY_PSK) {
+			if (creds[i].has_passphrase)
+				l_settings_set_string(settings, "Security",
+					"Passphrase", creds[i].passphrase);
+			else
+				l_settings_set_bytes(settings, "Security",
+						"PreSharedKey", creds[i].psk,
+                                                sizeof(creds[i].psk));
+		}
 
 		l_debug("Storing credential for '%s(%s)'", ssid,
 						security_to_str(security));
-
-		if (security == SECURITY_PSK)
-			l_settings_set_bytes(settings, "Security",
-						"PreSharedKey", creds[i].psk,
-                                                sizeof(creds[i].psk));
-
 		storage_network_sync(security, ssid, settings);
-		l_settings_free(settings);
 	}
 }
 