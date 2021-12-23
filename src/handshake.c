illiliti/eiwd

commit f24cfa481b0c70c605bcccf5cce77b8328c90766

handshake: Add setter for vendor IEs
Some connections, like Hotspot require additional IEs to be used during
the Association.  These are now passed as 'extra_ies' when invoking
netdev_connect, however they are also needed during ReAssociation and FT
to such APs.

Additionally, it may be that Hotspot-enabled APs will start utilizing
FILS or SAE.  In these cases the extra_ies need to be accounted for
somehow, either by making a copy in handshake_state, netdev, or the
auth_proto itself.  Similarly, P2P which heavily uses vendor IEs can be
used over SAE in the future.

Since a copy of these IEs is needed, might as well store them in
handshake_state itself for easy book-keeping by network/station.

src/handshake.c
@@ -297,6 +297,33 @@ void handshake_state_set_fte(struct handshake_state *s, const uint8_t *fte)
 	replace_ie(&s->fte, fte);
 }
 
+void handshake_state_set_vendor_ies(struct handshake_state *s,
+					const struct iovec *iov,
+					size_t n_iovs)
+{
+	size_t i;
+	size_t len;
+
+	l_free(s->vendor_ies);
+	s->vendor_ies = NULL;
+
+	if (n_iovs == 0) {
+		s->vendor_ies_len = 0;
+		return;
+	}
+
+	for (i = 0, len = 0; i < n_iovs; i++)
+		len += iov[i].iov_len;
+
+	s->vendor_ies_len = len;
+	s->vendor_ies = l_malloc(len);
+
+	for (i = 0, len = 0; i < n_iovs; i++) {
+		memcpy(s->vendor_ies + len, iov[i].iov_base, iov[i].iov_len);
+		len += iov[i].iov_len;
+	}
+}
+
 void handshake_state_set_kh_ids(struct handshake_state *s,
 				const uint8_t *r0khid, size_t r0khid_len,
 				const uint8_t *r1khid)

src/handshake.h
@@ -92,6 +92,8 @@ struct handshake_state {
 	uint8_t *supplicant_rsnxe;
 	uint8_t *mde;
 	uint8_t *fte;
+	uint8_t *vendor_ies;
+	size_t vendor_ies_len;
 	enum ie_rsn_cipher_suite pairwise_cipher;
 	enum ie_rsn_cipher_suite group_cipher;
 	enum ie_rsn_cipher_suite group_management_cipher;
@@ -178,6 +180,9 @@ void handshake_state_set_ssid(struct handshake_state *s,
 void handshake_state_set_mde(struct handshake_state *s,
 					const uint8_t *mde);
 void handshake_state_set_fte(struct handshake_state *s, const uint8_t *fte);
+void handshake_state_set_vendor_ies(struct handshake_state *s,
+					const struct iovec *iov,
+					size_t n_iovs);
 
 void handshake_state_set_kh_ids(struct handshake_state *s,
 				const uint8_t *r0khid, size_t r0khid_len,

commit 60366346fb37dce59007fafe4b359df25e27d1e8

handshake: Do not leak vendor_ies
Direct leak of 7 byte(s) in 1 object(s) allocated from:
    #0 0x7fd748ad00f8 in __interceptor_malloc (/usr/lib/gcc/x86_64-pc-linux-gnu/9.4.0/libasan.so.5+0x10c0f8)
    #1 0x688c21 in l_malloc ell/util.c:62
    #2 0x4beec7 in handshake_state_set_vendor_ies src/handshake.c:324
    #3 0x464e4e in station_handshake_setup src/station.c:1203
    #4 0x472a2f in __station_connect_network src/station.c:2975
    #5 0x473a30 in station_connect_network src/station.c:3078
    #6 0x4ed728 in network_connect_8021x src/network.c:1497

Fixes: f24cfa481b0c ("handshake: Add setter for vendor IEs")

src/handshake.c
@@ -115,6 +115,7 @@ void handshake_state_free(struct handshake_state *s)
 	l_free(s->fte);
 	l_free(s->fils_ip_req_ie);
 	l_free(s->fils_ip_resp_ie);
+	l_free(s->vendor_ies);
 
 	if (s->erp_cache)
 		erp_cache_put(s->erp_cache);