illiliti/eiwd

commit 7e9971661bcb6fac5f8bd6f2c013354c56d8b396

netdev: Append any vendor IEs from the handshake

src/netdev.c
@@ -269,6 +269,7 @@ static inline bool is_offload(struct handshake_state *hs)
 }
 
 static unsigned int netdev_populate_common_ies(struct netdev *netdev,
+					struct handshake_state *hs,
 					struct l_genl_msg *msg,
 					struct iovec *iov,
 					unsigned int n_iov,
@@ -287,6 +288,8 @@ static unsigned int netdev_populate_common_ies(struct netdev *netdev,
 
 	if (rm_enabled_capabilities)
 		l_genl_msg_append_attr(msg, NL80211_ATTR_USE_RRM, 0, NULL);
+
+	c_iov = iov_ie_append(iov, n_iov, c_iov, hs->vendor_ies);
 
 	return c_iov;
 }
@@ -2736,7 +2739,8 @@ static void netdev_sae_tx_associate(void *user_data)
 	n_used = iov_ie_append(iov, n_iov, n_used, hs->supplicant_ie);
 	n_used = iov_ie_append(iov, n_iov, n_used, hs->mde);
 	n_used = iov_ie_append(iov, n_iov, n_used, hs->supplicant_rsnxe);
-	n_used = netdev_populate_common_ies(netdev, msg, iov, n_iov, n_used);
+	n_used = netdev_populate_common_ies(netdev, hs, msg,
+							iov, n_iov, n_used);
 	mpdu_sort_ies(subtype, iov, n_used);
 
 	l_genl_msg_append_attrv(msg, NL80211_ATTR_IE, iov, n_used);
@@ -2772,6 +2776,7 @@ static void netdev_owe_tx_associate(struct iovec *owe_iov, size_t n_owe_iov,
 					void *user_data)
 {
 	struct netdev *netdev = user_data;
+	struct handshake_state *hs = netdev->handshake;
 	struct l_genl_msg *msg;
 	struct iovec iov[64];
 	unsigned int n_iov = L_ARRAY_SIZE(iov);
@@ -2781,7 +2786,7 @@ static void netdev_owe_tx_associate(struct iovec *owe_iov, size_t n_owe_iov,
 
 	msg = netdev_build_cmd_associate_common(netdev);
 
-	c_iov = netdev_populate_common_ies(netdev, msg, iov, n_iov, c_iov);
+	c_iov = netdev_populate_common_ies(netdev, hs, msg, iov, n_iov, c_iov);
 
 	if (!L_WARN_ON(n_iov - c_iov < n_owe_iov)) {
 		memcpy(iov + c_iov, owe_iov, sizeof(*owe_iov) * n_owe_iov);
@@ -2830,6 +2835,7 @@ static void netdev_fils_tx_associate(struct iovec *fils_iov, size_t n_fils_iov,
 					void *user_data)
 {
 	struct netdev *netdev = user_data;
+	struct handshake_state *hs = netdev->handshake;
 	struct l_genl_msg *msg;
 	struct iovec iov[64];
 	unsigned int n_iov = L_ARRAY_SIZE(iov);
@@ -2838,7 +2844,7 @@ static void netdev_fils_tx_associate(struct iovec *fils_iov, size_t n_fils_iov,
 				MPDU_MANAGEMENT_SUBTYPE_ASSOCIATION_REQUEST;
 
 	msg = netdev_build_cmd_associate_common(netdev);
-	c_iov = netdev_populate_common_ies(netdev, msg, iov, n_iov, c_iov);
+	c_iov = netdev_populate_common_ies(netdev, hs, msg, iov, n_iov, c_iov);
 
 	if (!L_WARN_ON(n_iov - c_iov < n_fils_iov)) {
 		memcpy(iov + c_iov, fils_iov, sizeof(*fils_iov) * n_fils_iov);
@@ -2968,7 +2974,7 @@ static struct l_genl_msg *netdev_build_cmd_connect(struct netdev *netdev,
 				0, NULL);
 
 	c_iov = iov_ie_append(iov, n_iov, c_iov, hs->mde);
-	c_iov = netdev_populate_common_ies(netdev, msg, iov, n_iov, c_iov);
+	c_iov = netdev_populate_common_ies(netdev, hs, msg, iov, n_iov, c_iov);
 
 	mpdu_sort_ies(subtype, iov, c_iov);
 
@@ -3849,6 +3855,7 @@ static int netdev_ft_tx_associate(struct iovec *ft_iov, size_t n_ft_iov,
 {
 	struct netdev *netdev = user_data;
 	struct auth_proto *ap = netdev->ap;
+	struct handshake_state *hs = netdev->handshake;
 	struct l_genl_msg *msg;
 	struct iovec iov[64];
 	unsigned int n_iov = L_ARRAY_SIZE(iov);
@@ -3858,7 +3865,7 @@ static int netdev_ft_tx_associate(struct iovec *ft_iov, size_t n_ft_iov,
 
 	msg = netdev_build_cmd_associate_common(netdev);
 
-	c_iov = netdev_populate_common_ies(netdev, msg, iov, n_iov, c_iov);
+	c_iov = netdev_populate_common_ies(netdev, hs, msg, iov, n_iov, c_iov);
 
 	if (!L_WARN_ON(n_iov - c_iov < n_ft_iov)) {
 		memcpy(iov + c_iov, ft_iov, sizeof(*ft_iov) * n_ft_iov);

commit fda946e070f798d56015ce7c408104acacb49171

netdev: iov_ie_append: Support iovecs with multiple IEs
iov_ie_append assumed that a single IE was being added and thus the
length of the IE could be extracted directly from the element.  However,
iov_ie_append was used on buffers which could contain multiple IEs
concatenated together, for example in handshake_state::vendor_ies.  Most
of the time this was safe since vendor_ies was NULL or contained a
single element, but would result in incorrect behavior in the general
case.  Fix that by changing iov_ie_append signature to take an explicit
length argument and have the caller specify whether the element is a
single IE or multiple.

Fixes: 7e9971661bcb ("netdev: Append any vendor IEs from the handshake")

src/netdev.c
@@ -209,7 +209,7 @@ static bool mac_per_ssid;
 
 static unsigned int iov_ie_append(struct iovec *iov,
 					unsigned int n_iov, unsigned int c,
-					const uint8_t *ie)
+					const uint8_t *ie, size_t len)
 {
 	if (L_WARN_ON(c >= n_iov))
 		return n_iov;
@@ -218,7 +218,7 @@ static unsigned int iov_ie_append(struct iovec *iov,
 		return c;
 
 	iov[c].iov_base = (void *) ie;
-	iov[c].iov_len = ie[1] + 2;
+	iov[c].iov_len = len;
 
 	return c + 1u;
 }
@@ -286,19 +286,22 @@ static unsigned int netdev_populate_common_ies(struct netdev *netdev,
 
 	extended_capabilities = wiphy_get_extended_capabilities(netdev->wiphy,
 								netdev->type);
-	c_iov = iov_ie_append(iov, n_iov, c_iov, extended_capabilities);
+	c_iov = iov_ie_append(iov, n_iov, c_iov, extended_capabilities,
+				IE_LEN(extended_capabilities));
 
 	rm_enabled_capabilities =
 		wiphy_get_rm_enabled_capabilities(netdev->wiphy);
-	c_iov = iov_ie_append(iov, n_iov, c_iov, rm_enabled_capabilities);
+	c_iov = iov_ie_append(iov, n_iov, c_iov, rm_enabled_capabilities,
+				IE_LEN(rm_enabled_capabilities));
 
 	if (rm_enabled_capabilities)
 		l_genl_msg_append_attr(msg, NL80211_ATTR_USE_RRM, 0, NULL);
 
-	c_iov = iov_ie_append(iov, n_iov, c_iov, hs->vendor_ies);
+	c_iov = iov_ie_append(iov, n_iov, c_iov,
+				hs->vendor_ies, hs->vendor_ies_len);
 
-	if (hs->fils_ip_req_ie)
-		c_iov = iov_ie_append(iov, n_iov, c_iov, hs->fils_ip_req_ie);
+	c_iov = iov_ie_append(iov, n_iov, c_iov, hs->fils_ip_req_ie,
+				IE_LEN(hs->fils_ip_req_ie));
 
 	return c_iov;
 }
@@ -2502,7 +2505,8 @@ static struct l_genl_msg *netdev_build_cmd_connect(struct netdev *netdev,
 
 	if (is_rsn) {
 		nl80211_append_rsn_attributes(msg, hs);
-		c_iov = iov_ie_append(iov, n_iov, c_iov, hs->supplicant_ie);
+		c_iov = iov_ie_append(iov, n_iov, c_iov, hs->supplicant_ie,
+					IE_LEN(hs->supplicant_ie));
 	}
 
 	if (is_rsn || hs->settings_8021x) {
@@ -2517,10 +2521,10 @@ static struct l_genl_msg *netdev_build_cmd_connect(struct netdev *netdev,
 
 	if (netdev->owe_sm) {
 		owe_build_dh_ie(netdev->owe_sm, owe_dh_ie, &dh_ie_len);
-		c_iov = iov_ie_append(iov, n_iov, c_iov, owe_dh_ie);
+		c_iov = iov_ie_append(iov, n_iov, c_iov, owe_dh_ie, dh_ie_len);
 	}
 
-	c_iov = iov_ie_append(iov, n_iov, c_iov, hs->mde);
+	c_iov = iov_ie_append(iov, n_iov, c_iov, hs->mde, IE_LEN(hs->mde));
 	c_iov = netdev_populate_common_ies(netdev, hs, msg, iov, n_iov, c_iov);
 
 	mpdu_sort_ies(subtype, iov, c_iov);
@@ -3267,9 +3271,11 @@ static void netdev_sae_tx_associate(void *user_data)
 
 	msg = netdev_build_cmd_associate_common(netdev);
 
-	n_used = iov_ie_append(iov, n_iov, n_used, hs->supplicant_ie);
-	n_used = iov_ie_append(iov, n_iov, n_used, hs->mde);
-	n_used = iov_ie_append(iov, n_iov, n_used, hs->supplicant_rsnxe);
+	n_used = iov_ie_append(iov, n_iov, n_used, hs->supplicant_ie,
+						IE_LEN(hs->supplicant_ie));
+	n_used = iov_ie_append(iov, n_iov, n_used, hs->mde, IE_LEN(hs->mde));
+	n_used = iov_ie_append(iov, n_iov, n_used, hs->supplicant_rsnxe,
+					IE_LEN(hs->supplicant_rsnxe));
 	n_used = netdev_populate_common_ies(netdev, hs, msg,
 							iov, n_iov, n_used);
 	mpdu_sort_ies(subtype, iov, n_used);