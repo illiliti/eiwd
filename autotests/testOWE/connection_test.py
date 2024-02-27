9f18bf402ca02303802064662981f6036e5ff712
James Prestwood <prestwoj@gmail.com>
2024-02-27 10:34:05 -0800

    auto-t: add OWE test for auto default group

--- autotests/testOWE/connection_test.py
+++ autotests/testOWE/connection_test.py
@@ -64,6 +64,39 @@ class Test(unittest.TestCase):
         self.assertRaises(Exception, testutil.test_ifaces_connected,
                           (hapd0.ifname, device.name, True, True))
 
+    def test_auto_default_group(self):
+        wd = IWD()
+        hapd = HostapdCLI(config='ssidOWE-1.conf')
+
+        devices = wd.list_devices(1)
+        device = devices[0]
+
+        device.get_ordered_network('ssidOWE')
+
+        device.connect_bssid(hapd.bssid)
+
+        condition = 'obj.state == DeviceState.connected'
+        wd.wait_for_object_condition(device, condition)
+
+        # Should have rejected group 20, but still connected on 19
+        self.assertEqual(device.event_ocurred("ecc-group-rejected"), True)
+
+        testutil.test_iface_operstate()
+        testutil.test_ifaces_connected(device.name, hapd.ifname)
+
+        device.disconnect()
+
+        device.connect_bssid(hapd.bssid)
+
+        condition = 'obj.state == DeviceState.connected'
+        wd.wait_for_object_condition(device, condition)
+
+        # IWD should have used the default group, no rejection
+        self.assertEqual(device.event_ocurred("ecc-group-rejected"), False)
+
+        testutil.test_iface_operstate()
+        testutil.test_ifaces_connected(device.name, hapd.ifname)
+
     @classmethod
     def setUpClass(cls):
         pass
